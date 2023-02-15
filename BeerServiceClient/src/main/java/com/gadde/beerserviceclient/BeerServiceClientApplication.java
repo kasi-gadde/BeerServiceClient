b3c751d5fc8025c2421eddca3d03fc53b61cdfce
kasi-gadde <kasi.gadde@gmail.com>
2023-02-15 07:48:42 -0500

    Initial Commit.

---
+++ BeerServiceClient/src/main/java/com/gadde/beerserviceclient/BeerServiceClientApplication.java
@@ -0,0 +1,13 @@
+package com.gadde.beerserviceclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerServiceClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerServiceClientApplication.class, args);
+	}
+
+}
---
+++ BeerServiceClient/src/test/java/com/gadde/beerserviceclient/BeerServiceClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gadde.beerserviceclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerServiceClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
